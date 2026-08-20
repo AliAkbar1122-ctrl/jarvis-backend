d656cd03a887277bbd2224ebb5cc81771cbee510
AliAkbar1122-ctrl <muhammadnasrullahkhan77@gmail.com>
2026-08-20 20:39:46 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const { google } = require('googleapis');
 
 const app = express();
 app.use(express.json());
-app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 const oauth2Client = new google.auth.OAuth2(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
